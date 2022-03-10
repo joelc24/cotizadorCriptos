1f84f1d97d9a685ae936f11f91286c1d6fe41492
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-03-10 09:23:22 -0500

    Styled Components al Select

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -2,13 +2,26 @@ import styled from '@emotion/styled'
 
 const Label = styled.label`
     color: #fff;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
+    margin-bottom: 20px;
 `
 
 const useSelectMonedas = (label, opciones) => {
     const SelectMonenedas = () =>(
         <>
             <Label>{label}</Label>
-            <select>
+            <Select>
                 <option value="">Seleccione</option>
 
                 {opciones.map( opcion => (
@@ -17,7 +30,7 @@ const useSelectMonedas = (label, opciones) => {
                         value={opcion.id}
                     >{opcion.nombre}</option>
                 ) )}
-            </select>
+            </Select>
         </>
     )
 
