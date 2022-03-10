9fbb5c9d6551fe969aa46bc4ce3255c2c76d54d6
Joel <libra-marriaga@hotmail.com>
2022-03-09 22:38:14 -0500

    Iniciando Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
+
+
+
+function App() {
+
+  return (
+    <Heading>Desde App</Heading>
+  )
+}
+
+export default App
