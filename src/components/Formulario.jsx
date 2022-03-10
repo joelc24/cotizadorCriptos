85402941d8d78d2fb9e83dc02f38165d09f17dc5
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-03-10 10:22:26 -0500

    Formateando un  Array para pasarlo como opciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import styled from "@emotion/styled";
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
@@ -21,15 +21,26 @@ const InputSubmit = styled.input`
 `
 const Formulario = () => {
 
+    const [criptos, setCriptos] = useState([])
+
     const [ moneda, SelectMonenedas ] = useSelectMonedas('Elige Tu moneda', monedas)
     
     useEffect(()=>{
         const consultaAPI = async () =>{
-            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD" 
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD" 
 
             const respuesta = await fetch(url)
             const resultado =  await respuesta.json()
-            console.log(resultado.Data)
+            
+
+            const arrayCriptos = resultado.Data.map( cripto =>{
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto
+            } )
+            setCriptos(arrayCriptos)
         }
 
         consultaAPI()
